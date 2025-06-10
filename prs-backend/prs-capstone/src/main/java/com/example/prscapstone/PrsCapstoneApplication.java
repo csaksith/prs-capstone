90d43a8fe34bfc7fc8f72051625dd3f010691c3a
csaksith <saksitca@mail.uc.edu>
2025-06-10 12:31:48 -0400

    initial backend setup: create user entity, userrepository and database config

---
+++ prs-backend/prs-capstone/src/main/java/com/example/prscapstone/PrsCapstoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.prscapstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrsCapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrsCapstoneApplication.class, args);
+	}
+
+}
---
+++ prs-backend/prs-capstone/src/test/java/com/example/prscapstone/PrsCapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prscapstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrsCapstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
